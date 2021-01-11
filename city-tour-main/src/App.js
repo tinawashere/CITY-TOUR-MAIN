a212b04d7633b140f6ed457ff25ee7f16c109039
tinawashere <jblanding1998@gmail.com>
2021-01-10 22:10:07 -0500

    Add files via upload

---
+++ city-tour-main/src/App.js
@@ -0,0 +1,16 @@
+
+import React, {Component} from 'react'
+
+
+class App extends Component {
+  render () {
+    return (
+    <main>
+      <Navbar/>
+      <TourList/>
+    </main>
+    );
+  }
+}
+
+export default App;
